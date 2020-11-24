4dc4d1223f9941e31612333db325f9ebf92de6b1
Jan Altensen <info@stricted.net>
2020-11-24 14:36:47 +0400

    common: Copy vulkan to hw/vulkan.exynos5 manually
    * Symlinks are broken for some reason

--- gts2-common/Android.mk
+++ gts2-common/Android.mk
@@ -28,9 +28,7 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/egl
 
 SYMLINKS := $(TARGET_OUT)/vendor
 $(SYMLINKS):
-	@echo "Symlink: vulkan.exynos5.so"
 	@mkdir -p $@/lib/hw
-	$(hide) ln -sf egl/libGLES_mali.so $@/lib/hw/vulkan.exynos5.so
 	@echo "Symlink: libOpenCL.so"
 	$(hide) ln -sf egl/libGLES_mali.so $@/lib/libOpenCL.so
 	@echo "Symlink: libOpenCL.so.1"
